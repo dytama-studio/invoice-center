7d6e9e263289d2bdf8a116a2a042fe0a9703788f
aditya.septama <risingadityahalim18@gmail.com>
2026-02-12 22:33:20 +0700

    move_ke_local

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,19 @@
 import "dotenv/config";
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@/db/schema";
+import { Pool } from "pg";
 
-const sql = neon(process.env.DATABASE_URL!);
+const pool:any = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-export const db = drizzle(sql, { schema });
+
+export const db = drizzle(pool, { schema });
+
+
+
+// const sql = neon(process.env.DATABASE_URL!);
+
+// export const db = drizzle(sql, { schema });
